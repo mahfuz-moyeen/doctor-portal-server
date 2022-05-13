b3329a3116448e97c0dcf6d3ac5a48e643f17131
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-05-14 00:32:39 +0600

    heroku server

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 const app = express();
 const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aij94.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const appointmentsCollection = client.db("doctor_portal").collection("appointments");
+
+        //get appointments
+        app.get('/appointments', async (req, res) => {
+            const query = {}
+            const cursor = appointmentsCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.send(appointments);
+        })
+
+    }
+    finally {
+        
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('doctor portal server on');
