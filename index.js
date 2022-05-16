6f082cc1e9a166ccf1563b3fff98f32475801f02
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-05-16 12:09:03 +0600

    add token

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 async function run() {
     try {
@@ -38,6 +52,7 @@ async function run() {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
             res.send({ result, token })
         })
+
 
         //get appointments
         app.get('/appointments', async (req, res) => {
@@ -81,13 +96,20 @@ async function run() {
             res.send({ success: true, result })
         })
 
+
         //get booking
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyToken, async (req, res) => {
             const email = req.query.email;
-            const query = { email: email }
-            const cursor = bookingCollection.find(query);
-            const booking = await cursor.toArray();
-            res.send(booking);
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                const query = { email: email }
+                const cursor = bookingCollection.find(query);
+                const booking = await cursor.toArray();
+                return res.send(booking);
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
         })
 
     }
