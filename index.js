c702bf5bdcc9ac933d9aba475bd9da4267e716f6
Mahfuz Zahan Moyeen <mahfuzzm10@gmail.com>
2024-08-26 02:45:48 +0600

    fix issue

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
 
 app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aij94.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f4jstaa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
